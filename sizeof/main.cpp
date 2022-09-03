3e72709c4a97f0263f053098ee24332d6ddb3516
Ashish kumar anand <ashishraj5296@gmail.com>
2022-09-03 18:57:45 +0530

    sizeof

---
+++ sizeof/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout<<"Size of Int , Float , Char , Double , String , Boolean :"<<endl;
+  cout<<"int= "<<sizeof(int)<<endl;
+  cout<<"Float= "<<sizeof(float)<<endl;
+  cout<<"Char= "<<sizeof(char)<<endl;
+  cout<<"Double= "<<sizeof(double)<<endl;
+  cout<<"String= "<<sizeof(string)<<endl;
+  cout<<"Boolean= "<<sizeof(string)<<endl;
+  cout<<char(90);
+    return 0;
+}
